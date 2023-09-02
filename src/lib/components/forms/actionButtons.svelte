6af2cab4060180431056cc3a63f8ac4579c6aa50
Juri Berlanda <juriberlanda@hotmail.com>
2023-09-02 13:38:14 +0200

    [Components] Make form/ActionButtons more flexible.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- src/lib/components/forms/actionButtons.svelte
+++ src/lib/components/forms/actionButtons.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
 	export let valid = true;
-	export let reset: () => void;
+	export let secondaryButtonLabel: string;
+
+	const dispatch = createEventDispatcher();
+	const onSecondaryClickHandler = () => dispatch('secondaryClick');
 </script>
 
 <div class="grid">
-	<button type="reset" class="secondary" on:click|preventDefault={reset}>Reset</button>
+	<button type="reset" class="secondary" on:click|preventDefault={onSecondaryClickHandler}>
+		{secondaryButtonLabel}
+	</button>
 	<button type="submit" disabled={!valid}>Save</button>
 </div>
