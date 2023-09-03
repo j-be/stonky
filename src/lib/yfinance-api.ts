366a992e087163a4294e9276012dd5cfa80bd0c6
Juri Berlanda <juriberlanda@hotmail.com>
2023-09-03 13:26:06 +0200

    [Cache] Use indexedDB as Cache for YFinance API.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- src/lib/yfinance-api.ts
+++ src/lib/yfinance-api.ts
@@ -1,22 +1,31 @@
 import { addDays, addHours, format } from 'date-fns';
+import { YFinanceDB } from './db';
+
 // const BASE_URL = 'https://query2.finance.yahoo.com/v8/finance/chart/DT';
 const BASE_URL = 'https://yfinance.great-horned-owl.dedyn.io/v8/finance/chart';
 
 export const fetchForDateString = async (dateString: string, symbol: string): Promise<number> => {
-	const key = `adjclose-${symbol}-${dateString}`;
-	const cached = Number(localStorage?.getItem(key));
+	let db: YFinanceDB | null = null;
+	try {
+		db = await YFinanceDB.init();
+	} catch (error) {
+		console.warn('Cannot init DB:', error);
+	}
+
+	const cached = await db?.get({ date: dateString, symbol });
 	if (cached) {
 		return Promise.resolve(cached);
 	}
+
 	const fetched = await fetchForDate(new Date(dateString), symbol);
-	if (fetched && localStorage) {
-		localStorage?.setItem(key, String(fetched));
+	if (fetched && db) {
+		db.save({ date: dateString, symbol, value: fetched });
 	}
 	return fetched;
 };
 
 export const fetchForNow = (symbol: string): Promise<number> => {
-	return fetchForDate(addDays(new Date(), -1), symbol);
+	return fetchForDateString(format(addDays(new Date(), -1), 'yyyy-MM-dd'), symbol);
 };
 
 const toEpoch = (date: Date): number => Math.floor(date.getTime() / 1000);
