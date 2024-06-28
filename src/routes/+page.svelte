4e3b8371d0e971c7a00bcbc4a6ec0987ce0f1437
Juri Berlanda <juriberlanda@hotmail.com>
2024-06-28 14:32:03 +0200

    [Refactor] Make overview its dedicated component.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,62 +1,13 @@
 <script lang="ts">
-	import { annualGrossStore, esppStore, exchangeRateStore, rsuStore, stockPriceStore, taxStore } from '$lib/stores';
+	import { esppStore, rsuStore } from '$lib/stores';
 	import Stocks from '$lib/components/stocks.svelte';
 	import Disclaimer from '$lib/components/disclaimer.svelte';
-	import Money from '$lib/components/money.svelte';
-
-	let currentPrice = NaN;
-	let exchangeRate = NaN;
-
-	$: exchangeRate = $exchangeRateStore;
-	$: currentPrice = $stockPriceStore;
-	$: annualGross = $annualGrossStore;
+	import Overview from '$lib/components/overview.svelte';
 </script>
 
 <Disclaimer />
 
-<article class="overview">
-	<table>
-		<tbody>
-			<tr>
-				<th>Stock price</th>
-				<td>
-					{#if !isNaN(currentPrice)}
-						{currentPrice} $
-					{:else}
-						<span aria-busy="true" />
-					{/if}
-				</td>
-			</tr>
-			<tr>
-				<th>Exchange rate</th>
-				<td>
-					{#if !isNaN(exchangeRate)}
-						{exchangeRate} <sup>$</sup>/<sub>€</sub>
-					{:else}
-						<span aria-busy="true" />
-					{/if}
-				</td>
-			</tr>
-			<tr>
-				<th>Income tax</th>
-				<td>
-					{$taxStore * 100} %
-					<a href="/settings" class="icon settings">&#8288;</a>
-				</td>
-			</tr>
-			<tr>
-				<th>RSU annual</th>
-				<td>
-					{#if !isNaN(annualGross)}
-						<Money value={annualGross} />
-					{:else}
-						<span aria-busy="true" />
-					{/if}
-				</td>
-			</tr>
-		</tbody>
-	</table>
-</article>
+<Overview />
 
 {#each [$esppStore, $rsuStore] as stocks}
 	{#each stocks as stock, id}
