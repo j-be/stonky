969513c4dec845c8c787ad031cf572351cbaf38a
Juri Berlanda <juriberlanda@hotmail.com>
2024-09-13 17:45:40 +0200

    [Layout] Extract StocksHeader.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

---
+++ src/lib/components/stocks/stocksHeader.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { StockType } from '$lib/model';
+	import Money from '../money.svelte';
+
+	export let id: number;
+	export let type: StockType;
+	export let count: number;
+	export let value: number;
+</script>
+
+<div class="title">
+	<a href="/edit/{type}/{id}" class="icon edit">&#8288;</a>
+	<span>
+		<span class="uppercase">{type}</span>
+		{count}
+	</span>
+</div>
+<div><Money {value} deductTax={type == 'rsu'} /></div>
