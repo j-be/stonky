8992f5e71676e440f7ddc70bc09fe004fc11f65a
Juri Berlanda <juriberlanda@hotmail.com>
2024-10-18 14:09:21 +0200

    [Layout] Minor fixes in layout.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- src/lib/components/forms/actionButtons.svelte
+++ src/lib/components/forms/actionButtons.svelte
@@ -2,7 +2,7 @@
 	export let valid = true;
 </script>
 
-<div class="grid">
+<div class="action-buttons">
 	<button type="button" class="secondary" on:click={() => history.back()}>Cancel</button>
 	<button type="submit" disabled={!valid}>Save</button>
 </div>
